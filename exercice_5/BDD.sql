c3640b96d73d7ac0e33605f86cfc0f4bd2f49a10
LUKA <donluka94@gmail.com>
2024-11-24 01:46:39 +0100

    Add files via upload

---
+++ exercice_5/BDD.sql
@@ -0,0 +1,14 @@
+
+CREATE TABLE contact (
+id_contact VARCHAR(255) NOT NULL PRIMARY KEY,
+type_cours VARCHAR(255) NOT NULL,
+tranche_age VARCHAR(255) NOT NULL, 
+nom VARCHAR (255) NOT NULL, 
+prenom VARCHAR (255) NOT NULL, 
+age int NOT NULL, 
+email VARCHAR(255) NOT NULL, 
+telephone int NOT NULL, 
+sujet TEXT NOT NULL,
+date_contact DATE NOT NULL
+
+);
