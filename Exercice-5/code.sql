07f43678c7660934730e1c572d9263437223e6cc
fabrice-sama <fabriceertone.sama@eemi.com>
2024-11-30 18:53:41 +0100

    Create code.sql

---
+++ Exercice-5/code.sql
@@ -0,0 +1,13 @@
+CREATE TABLE priseContact (
+    identifiant INT AUTO_INCREMENT PRIMARY KEY,
+    sujet VARCHAR(255) NOT NULL,
+    type_cours VARCHAR(255) NOT NULL,
+    tranche_age VARCHAR(255) NOT NULL,
+    age INT(3) NOT NULL,
+    nom VARCHAR(255) NOT NULL,
+    prenom VARCHAR(255) NOT NULL,
+    email VARCHAR(255) NOT NULL,
+    telephone INT(13) NOT NULL,
+    message VARCHAR(MAX) NOT NULL,
+    date_contact DATE NOT NULL DEFAULT CURRENT_TIMESTAMP
+);
