44e93850c30ae7ffab531e9c7420c789df79dd66
fabrice-sama <fabriceertone.sama@eemi.com>
2025-01-17 15:33:35 +0100

    Update gestion_stock.sql

--- Exercices/programmation/stock.sql
+++ Exercices/programmation/stock.sql
@@ -1,1 +1,30 @@
+DROP DATABASE IF EXISTS gestion_stock;
 
+CREATE DATABASE gestion_stock;
+USE gestion_stock;
+
+CREATE TABLE produit (
+    code INT PRIMARY KEY,
+    designation VARCHAR(255),
+    prix VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE fournisseur (
+    numero INT PRIMARY KEY,
+    nom VARCHAR(255) NOT NULL,
+    adreesse VARCHAR(255) NOT NULL,
+);
+
+CREATE TABLE approvisionnement (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    prix_achat INT NOT NULL,
+    nb_produit INT NOT NULL,
+    FOREIGN KEY (id_fournisseur) REFERENCES fournisseur(numero) NOT NULL,
+    FOREIGN KEY (id_produit) REFERENCES produit(code) NOT NULL,
+);
+
+CREATE TABLE livraison (
+    id INT AUTO_INCREMENT PRIMARY KEY,
+    prix INT NOT NULL,
+    FOREIGN KEY (id_approvisionnement) REFERENCES approvisionnement(id) NOT NULL,
+);
