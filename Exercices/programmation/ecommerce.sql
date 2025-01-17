68de1bae700377f38151480072a4be28833812d0
fabrice-sama <fabriceertone.sama@eemi.com>
2025-01-17 14:12:46 +0100

    Update ecommerce.sql

--- Exercices/programmation/ecommerce.sql
+++ Exercices/programmation/ecommerce.sql
@@ -2,3 +2,9 @@ SELECT COUNT(*) FROM orders
 INNER JOIN order_items ON orders.id = orders_items.order_id
 INNER JOIN products ON order_items.product_id = products.id
 WHERE products.id = 2456;
+
+SELECT name FROM products WHERE stock = 0;
+
+SELECT id FROM orders WHERE order_date > ;
+
+SELECT name FROM CUSTOMERS WHERE email LIKE '%@gmail.com'
