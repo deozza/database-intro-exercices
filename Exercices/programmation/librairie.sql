738d3156b45cf11d8e334f94f860af5061404282
fabrice-sama <fabriceertone.sama@eemi.com>
2025-01-17 16:47:14 +0100

    Update librairie.sql

--- Exercices/programmation/librairie.sql
+++ Exercices/programmation/librairie.sql
@@ -1,4 +1,4 @@
 SELECT firstname, lastname FROM authors;
 SELECT firstname, lastname FROM editors WHERE name LIKE "%,%";
-SELECT id FROM loans WHERE return_date < ;
+SELECT id FROM loans WHERE return_date < CURRENT_DATE();
 SELECT title FROM books WHERE LEN(title) < 50;
