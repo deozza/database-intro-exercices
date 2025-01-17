8da16e3dd91a87e8f4a622210430d2b2b105f208
JulesGGn <125991856+JulesGGn@users.noreply.github.com>
2025-01-17 12:27:04 +0100

    Add files via upload

---
+++ Exercice3/Ecommerce.sql
@@ -0,0 +1,15 @@
+
+SELECT quantity
+FROM ORDER_ITEMS
+WHERE produit_id = 2456;
+
+SELECT name
+FROM PRODUCTS
+WHERE stock = 0;
+
+SELECT *
+FROM 
+
+SELECT *
+FROM CUSTOMER
+WHERE email LIKE '%@gmail.com';
